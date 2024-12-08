5be8ed26fc8c82a4cdc03e45373a50f63295f786
SoCLoN666 <kusminoleh96@gmail.com>
2024-12-08 17:18:31 +0200

    test

--- playwright-setup.config.ts
+++ playwright-setup.config.ts
@@ -72,6 +72,7 @@ export default defineConfig({
     //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
     // },
   ],
+  globalSetup: path.resolve(__dirname, "e2e/setup/global-setup.ts"),
 
   /* Run your local dev server before starting the tests */
   // webServer: {
