e1c6e4a8fdd1d266b644455655a8f03ec51197e7
SoCLoN666 <kusminoleh96@gmail.com>
2024-12-08 20:32:55 +0200

    test

--- playwright.config.ts
+++ playwright.config.ts
@@ -21,10 +21,19 @@ export default defineConfig({
   retries: process.env.CI ? 2 : 0,
   testMatch: "*.spec-e2e.ts",
   workers: process.env.CI ? 1 : undefined,
-  reporter: "html",
+  reporter: [
+    [
+      "html",
+      {
+        open: "never",
+        outputFolder: process.env.TEST_TAG ? "./test-report" : "./other-test-report",
+      },
+    ],
+  ],
   expect: {
     timeout: 30000,
   },
+
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://127.0.0.1:3000',
@@ -41,42 +50,5 @@ export default defineConfig({
       name: "chromium",
       use: { ...devices["Desktop Chrome"] },
     },
-
-    // {
-    //   name: "firefox",
-    //   use: { ...devices["Desktop Firefox"] },
-    // },
-
-    // {
-    //   name: "webkit",
-    //   use: { ...devices["Desktop Safari"] },
-    // },
-
-    /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: { ...devices['Pixel 5'] },
-    // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: { ...devices['iPhone 12'] },
-    // },
-
-    /* Test against branded browsers. */
-    // {
-    //   name: 'Microsoft Edge',
-    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
-    // },
-    // {
-    //   name: 'Google Chrome',
-    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-    // },
   ],
-
-  /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://127.0.0.1:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
 });
